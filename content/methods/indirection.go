5d0ac99e7b8fe600dd44c4d9a9f3f6cf53bb2f18
anilgkurian <anilgkurian@live.com>
2017-01-25 14:29:49 +0530

    Simplifying the pointer indirection example

--- content/methods/indirection.go
+++ content/methods/indirection.go
@@ -8,24 +8,24 @@ type Vertex struct {
 	X, Y float64
 }
 
-func (v *Vertex) Scale(f float64) {
-	v.X = v.X * f
-	v.Y = v.Y * f
+func (v *Vertex) PointerRecevierMethod() {
+	v.X = v.X * 10
+	v.Y = v.Y * 10
 }
 
-func ScaleFunc(v *Vertex, f float64) {
-	v.X = v.X * f
-	v.Y = v.Y * f
+func PointerFunction(v *Vertex) {
+	v.X = v.X + 1
+	v.Y = v.Y + 1
 }
 
 func main() {
 	v := Vertex{3, 4}
-	v.Scale(2)
-	ScaleFunc(&v, 10)
+	v.PointerRecevierMethod()
+	PointerFunction(&v)
 
-	p := &Vertex{4, 3}
-	p.Scale(3)
-	ScaleFunc(p, 8)
+	p := &Vertex{5, 6}
+	p.PointerRecevierMethod()
+	PointerFunction(p)
 
 	fmt.Println(v, p)
 }
